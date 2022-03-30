5c6deb3d054f80c5a0fe24d593d56f9d0642922e
axelsvenn <mrr.axelsven@gmail.com>
2022-03-30 19:22:05 +0300

    second task

--- main_program.py
+++ main_program.py
@@ -1,1 +1,24 @@
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    param = {"title": "Заготовка"
+             }
+    return render_template('index.html', **param)
+
+
+@app.route("/training/<prof>")
+def training(prof):
+    param = {"title": "Тренировки в полёте",
+             "prof": prof
+             }
+    return render_template('training.html', **param)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
