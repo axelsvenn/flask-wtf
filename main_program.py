36fdd2bd5666dec375cac3023952d8cd283cbe5b
axelsvenn <mrr.axelsven@gmail.com>
2022-03-30 19:14:26 +0300

    ochko

--- main_program.py
+++ main_program.py
@@ -1,1 +1,16 @@
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    param = {"title": "Заготовка"
+             }
+    return render_template('index.html', **param)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
