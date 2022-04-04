44585885143a24c8e298687a81ef625e742cf867
axelsvenn <mrr.axelsven@gmail.com>
2022-04-04 16:31:56 +0300

    sixth task

--- main_program.py
+++ main_program.py
@@ -1,1 +1,15 @@
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+users = ['Капитан Грант', 'Сергей Садовников', 'Джек Воробей',
+         'Ридли Скотт', 'Пятнадцатилетний капитан', 'Моби Дик']
+
+
+@app.route('/distribution')
+def distribution():
+    return render_template('index.html', users=users)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
