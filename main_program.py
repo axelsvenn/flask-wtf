11fadc351c47631508d2303be1a158a0286bcd09
axelsvenn <mrr.axelsven@gmail.com>
2022-04-06 15:12:51 +0300

    seventh task

--- main_program.py
+++ main_program.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/table_param/<sex>/<age>')
 def table_param(sex, age):
-    return render_template('index.html', sex=sex, age=age)
+    if sex == "male":
+        photo, color = (url_for('static', filename='img/adult.png'), "#0000CD") if int(age) >= 21 else \
+            (url_for('static', filename='img/young'), "#4169E1")
+    else:
+        photo, color = (url_for('static', filename='img/adult.png'), "#FF4500") if int(age) >= 21 else \
+            (url_for('static', filename='img/young'), "#FFA07A")
+
+    return render_template('index.html', photo=photo, color=color)
 
 
 if __name__ == '__main__':
