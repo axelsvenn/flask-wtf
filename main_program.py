75c8326656087b7ac2009249532a2577f9bc0b2a
axelsvenn <mrr.axelsven@gmail.com>
2022-04-06 18:26:36 +0300

    eighth task but i don't finish this

--- main_program.py
+++ main_program.py
@@ -1,1 +1,13 @@
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/carousel')
+def carousel():
+    # file_carousel = lambda x: url_for('static', filename=f'img/{x}')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
