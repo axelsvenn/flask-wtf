aca23f7ba72378fc72a6fd733be9fdaf84fc9661
axelsvenn <mrr.axelsven@gmail.com>
2022-04-06 14:41:28 +0300

    seventh task

--- main_program.py
+++ main_program.py
@@ -1,1 +1,12 @@
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/table_param/<sex>/<age>')
+def table_param(sex, age):
+    return render_template('index.html', sex=sex, age=age)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
