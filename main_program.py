35d9284f3147771fd93d282853d3e2ed982d7d26
axelsvenn <mrr.axelsven@gmail.com>
2022-04-08 16:51:26 +0300

    eighth task but i don't finish this

--- main_program.py
+++ main_program.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
-app = Flask(__name__)
+app, i = Flask(__name__), 1
 
 
-@app.route('/carousel')
+@app.route('/carousel', methods=['POST', 'GET'])
 def carousel():
-    # file_carousel = lambda x: url_for('static', filename=f'img/{x}')
-    return render_template('index.html')
+    params = {"css_file": url_for('static', filename='css/style.css')}
+    if request.method == 'GET':
+        return render_template('carousel.html', **params)
+
+    elif request.method == 'POST':
+        with open(f"static/img/photo{i}.png", 'wb') as fd:
+            f = request.files["file"]
+            fd.write(f.read())
+            global i
+            i += 1
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
