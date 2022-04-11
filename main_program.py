a493e6e95ab9720d7a4a0f15f94822134964dca8
axelsvenn <mrr.axelsven@gmail.com>
2022-04-11 14:14:40 +0300

    eighth task but i don't finish this

--- main_program.py
+++ main_program.py
@@ -5,7 +5,7 @@ app, i = Flask(__name__), 1
 
 @app.route('/carousel', methods=['POST', 'GET'])
 def carousel():
-    params = {"css_file": url_for('static', filename='css/style.css', "photos": []}
+    params = {"css_file": url_for('static', filename='css/style.css'), "photos": []}
     if request.method == 'GET':
         return render_template('carousel.html', **params)
 
